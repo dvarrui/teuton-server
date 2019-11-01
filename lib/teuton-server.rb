7df534e6e984b071a942a98d6d4dddd4bc79ae10
David <dvarrui@protonmail.com>
2019-11-01 20:59:07 +0000

    U

--- lib/teuton-server.rb
+++ lib/teuton-server.rb
@@ -8,17 +8,21 @@ class TeutonServer
     loop {
       exit unless File.exists? state
       client = server.accept        # Wait for a client to connect
-      msg = "teuton play 01 => grade  0%"
-      puts "[#{Time.now}] #{msg}"
-      show_client client
-      client.puts("[teuton-server] #{msg}")
-      client.close
+      respond_to_client client
     }
   end
 
   def show_server(server)
     puts "[teuton-server] Running..."
     puts " └── #{server.addr}"
+  end
+
+  def respond_to_client(client)
+    msg = "teuton play 01 => grade  0%"
+    puts "[#{Time.now}] #{msg}"
+    show_client client
+    client.puts("[teuton-server] #{msg}")
+    client.close
   end
 
   def show_client(client)
