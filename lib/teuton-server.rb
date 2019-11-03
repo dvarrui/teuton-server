924f47807e7d19677504966c4014e6ee0a966480
David <dvarrui@protonmail.com>
2019-11-03 16:03:27 +0000

    U

--- lib/teuton-server.rb
+++ lib/teuton-server.rb
@@ -1,8 +1,9 @@
+require 'fileutils'
 require_relative 'server/input_loader'
 require_relative 'server/service_manager'
 
 module TeutonServer
-  def self.init(args)
+  def self.start(args)
     param = InputLoader.read_input_args(args)
     ServiceManager.start_services(param)
   end
@@ -12,6 +13,7 @@ module TeutonServer
     puts "    teuton-server [help|version] [PATH/TO/server.yaml]"
     puts "Params:"
     puts "    help       , show this help"
+    puts "    init       , Create server.yaml"
     puts "    version    , show current version"
     puts "    configfile , YAML server configuration file"
     exit 0
@@ -19,6 +21,14 @@ module TeutonServer
 
   def self.show_version
     puts "teuton-server (version 0.0.1)"
+    exit 0
+  end
+
+  def self.init(arg)
+    src = File.join(File.dirname(__FILE__), 'server', 'files', 'server.yaml')
+    dest = File.join( 'server.yaml')
+    FileUtils.cp(src, dest)
+    puts "[ teuton-server ] Init done!"
     exit 0
   end
 end
