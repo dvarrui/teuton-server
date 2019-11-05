27ad4d7b1bf8d40fe4ab788a772572990a68205b
David <dvarrui@protonmail.com>
2019-11-05 11:46:09 +0000

    U

--- lib/server/service.rb
+++ lib/server/service.rb
@@ -31,17 +31,19 @@ class Service
   end
 
   def respond_to_client(client, param, action)
+    #puts "                 ADDR        : #{client.addr}"
+    #puts "                 PEERADDR    : #{client.peeraddr}"
     puts "teuton-server => working...   service[#{param[:client][:id]}]"
     src = "#{param[:server][:ip]}:#{param[:server][:port]}"
     dest = "#{param[:client][:ip]}:#{client.peeraddr[1]}"
     tab = '                 '
-    puts tab + "Members    : #{param[:client][:members]}"
-    puts tab + "Connection : #{src} -> #{dest}"
-    #puts "                 ADDR        : #{client.addr}"
-    #puts "                 PEERADDR    : #{client.peeraddr}"
-    puts tab + "Timestamp  : #{action[:timestamp]}"
-    puts tab + "Action     : #{action[:cmd]}"
-    puts tab + "Status     : #{action[:status]}"
+
+    output = tab + "Members    : #{param[:client][:members]}\n" +
+             tab + "Connection : #{src} -> #{dest}\n" +
+             tab + "Timestamp  : #{action[:timestamp]}\n" +
+             tab + "Action     : #{action[:cmd]}\n" +
+             tab + "Status     : #{action[:status]}\n"
+    puts output 
     client.puts("Connection : #{src} -> #{dest} ")
     client.puts("Timestamp  : #{action[:timestamp]}")
     client.puts("Action     : #{action[:cmd]}")
