90876a0a4ec5702b0022d6216cc4d89a8aa4a5d9
David <dvarrui@protonmail.com>
2019-11-13 10:19:31 +0000

    U making private methods inside ServiceManager module

--- docs/installation.md
+++ docs/installation.md
@@ -4,4 +4,4 @@
 > **WARNING**: First, ensure [Teuton Software](https://github.com/teuton-software/teuton) is installed.
 
 * `gem install teuton-server`, to install TeutonServer.
-* `teuton-server version`, wil display TeutonServer version.
+* `teuton-server version`, display TeutonServer version.
--- lib/teuton-server/service_manager.rb
+++ lib/teuton-server/service_manager.rb
@@ -7,43 +7,47 @@ require_relative 'service'
 # * service 2 listen on port PORT+2, client 2 requests.
 # * etc.
 module ServiceManager
-  # Start one service for every client.
-  # @param app_param [Hash] Application configuration params
-  # @return [Exit status] Exit 0 = OK. Exit 1 = ERROR
-  def self.start_services(app_param)
-    show_starting(app_param)
-    services_param = split_app_param_into_services_param(app_param)
-    services = []
-    begin
-      services_param.each do |param|
-        services << Thread.new{ Service.new.run(param) }
+  class << self
+    # Start one service for every client.
+    # @param app_param [Hash] Application configuration params
+    # @return [Exit status] Exit 0 = OK. Exit 1 = ERROR
+    def start_services(app_param)
+      show_starting(app_param)
+      services_param = split_app_param_into_services_param(app_param)
+      services = []
+      begin
+       services_param.each do |param|
+          services << Thread.new{ Service.new.run(param) }
+        end
+        services.each { |service| service.join }
+      rescue SystemExit, Interrupt
+        puts Rainbow("\nteuton-server => Closing...").bright
+        exit 0
       end
-      services.each { |service| service.join }
-    rescue SystemExit, Interrupt
-      puts Rainbow("\nteuton-server => Closing...").bright
-      exit 0
     end
-  end
 
-  def self.show_starting(param)
-    puts Rainbow("teuton-server => Starting...").bright
-    puts "                 Configfile : #{param[:server][:configfile]}"
-    puts "                 Listen on  : #{param[:server][:ip]}:#{param[:server][:port]}"
-    puts "                 Test list  : #{param[:server][:testunits].join(',')}"
-    puts Rainbow("                 (CTRL+C to exit)").bright.yellow
-  end
+    private
 
-  def self.split_app_param_into_services_param(app_param)
-    services_param = []
-    app_param[:clients].each_with_index do |client, index|
-      param = { server: {}, client: {} }
-      param[:server].merge! app_param[:server] if app_param[:server]
-      param[:server][:hostname] = param[:server][:ip]
-      param[:client].merge! client
-      param[:client][:id] = index + 1
-      param[:server][:port] += param[:client][:id]
-      services_param << param
+    def show_starting(param)
+      puts Rainbow("teuton-server => Starting...").bright
+      puts "                 Configfile : #{param[:server][:configfile]}"
+      puts "                 Listen on  : #{param[:server][:ip]}:#{param[:server][:port]}"
+      puts "                 Test list  : #{param[:server][:testunits].join(',')}"
+      puts Rainbow("                 (CTRL+C to exit)").bright.yellow
     end
-    services_param
+
+    def split_app_param_into_services_param(app_param)
+      services_param = []
+      app_param[:clients].each_with_index do |client, index|
+        param = { server: {}, client: {} }
+        param[:server].merge! app_param[:server] if app_param[:server]
+        param[:server][:hostname] = param[:server][:ip]
+        param[:client].merge! client
+        param[:client][:id] = index + 1
+        param[:server][:port] += param[:client][:id]
+        services_param << param
+      end
+      services_param
+    end
   end
 end
